840b6cd777a795633859fc5c12aee81c30ffb0bf
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-08 21:50:52 +0900

    setting_searchController

--- SubwayStation/SubwayViewController.swift
+++ SubwayStation/SubwayViewController.swift
@@ -1,17 +1,27 @@
 //
-//  SubwayViewController.swift
+//  StationSearchViewController.swift
 //  SubwayStation
 //
 //  Created by 양성혜 on 2022/11/08.
 //
 
-import Foundation
 import UIKit
+import SnapKit
 
-class SubwayController: UIViewController{
+class StationSearchViewController: UIViewController{
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+   
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "지하철 도착 정보"
         
-        view.backgroundColor = .red
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "지하철 역을 입력해주세요."
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        navigationItem.searchController = searchController
     }
+    
+    
 }
