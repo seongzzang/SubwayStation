a55e046bd90c752e9d581e02119eac4124ffd49e
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-11-08 21:34:33 +0900

    setting_scene

---
+++ SubwayStation/SubwayViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SubwayViewController.swift
+//  SubwayStation
+//
+//  Created by 양성혜 on 2022/11/08.
+//
+
+import Foundation
+import UIKit
+
+class SubwayController: UIViewController{
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
