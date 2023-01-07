0ace3ad363bb5711adc0e323985330e4dced274b
seongzzang <87599027+seongzzang@users.noreply.github.com>
2023-01-07 21:38:54 +0900

    startView_update

---
+++ SubwayStation/StartViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StartViewController.swift
+//  SubwayStation
+//
+//  Created by 양성혜 on 2023/01/07.
+//
+
+import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    private lazy var imageView: UIImage {
+       let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupLayout()
+    }
+    
+    
+}
+
+private class StartViewController {
+    func setupLayout(){
+        [UIImageView]
+    }
+}
